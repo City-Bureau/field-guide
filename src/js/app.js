72e30658e156104da841c9a5745a6ec376456cb8
pjsier <pjsier@gmail.com>
2018-11-18 17:57:41 -0500

    Update styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+function expandCollapse() {
+  document.querySelectorAll(".toggle").forEach((el) => {
+    el.addEventListener("click", () => el.classList.toggle("active"));
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM ready");
+  expandCollapse();
 });
