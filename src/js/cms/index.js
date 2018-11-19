872fb76ea50caf1b5570a4b8afee0347862476a2
pjsier <pjsier@gmail.com>
2018-11-19 13:04:58 -0500

    Fix template, sidebar

--- src/js/cms/index.js
+++ src/js/cms/index.js
@@ -5,4 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
   window.CMS.registerPreviewTemplate("post", PostTemplate);
   window.CMS.registerPreviewTemplate("page", PostTemplate);
   window.CMS.registerPreviewTemplate("main", PostTemplate);
+  window.CMS.registerPreviewTemplate("attendance", PostTemplate);
+  window.CMS.registerPreviewTemplate("before-you-begin", PostTemplate);
+  window.CMS.registerPreviewTemplate("on-assignment", PostTemplate);
+  window.CMS.registerPreviewTemplate("going-public", PostTemplate);
 });
