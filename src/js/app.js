5b3601159e43c86dba85eda2b76eb25b318ba8d6
pjsier <pjsier@gmail.com>
2018-11-26 15:49:06 -0600

    Update toggling

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 function expandCollapse() {
   document.querySelectorAll(".toggle").forEach((el) => {
-    el.addEventListener("click", () => el.classList.toggle("active"));
+    el.addEventListener("click", () => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
+      } else {
+        document.querySelectorAll(".active").forEach((el) => el.classList.remove("active"));
+        el.classList.add("active");
+      }
+    });
   });
 }
 
